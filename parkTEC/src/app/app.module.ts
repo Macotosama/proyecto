59425eae2f8f84a1c0d31ff756566b252d15b91c
Macotosama <jtn1999@outlook.com>
2022-04-27 20:39:15 -0600

    update

--- parkTEC/src/app/app.module.ts
+++ parkTEC/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DtoUsuarioComponent } from './controler/DTO/dto-usuario/dto-usuario.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CookieService } from 'ngx-cookie-service';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { InicioComponent } from './interfaz/inicio/inicio.component';
 
 @NgModule({
@@ -16,9 +24,15 @@ import { InicioComponent } from './interfaz/inicio/inicio.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- parkTEC/src/app/interfaz/inicio/inicio.component.ts
+++ parkTEC/src/app/interfaz/inicio/inicio.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  contrasena = '';
+  hide = true;
 
   constructor() { }
 
