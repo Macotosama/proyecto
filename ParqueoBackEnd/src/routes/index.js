3da98467ef9ac4fa9e3d92a3d77c223c3e746cd3
Macotosama <jtn1999@outlook.com>
2022-05-09 21:11:40 -0600

    update

---
+++ ParqueoBackEnd/src/routes/index.js
@@ -0,0 +1,48 @@
+const {Router} = require('express');
+const {db} = require('../firebase');
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const querySnapshot = await db.collection('contacts').get();
+    const contacts = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+    console.log(contacts)
+    res .send('Hello');
+});
+
+router.post('/new-contac', async (req, res) => {
+    const {firstname, lastname, email, phone} = req.body
+
+    await db.collection('contacts').add({
+        firstname,
+        lastname,
+        email,
+        phone
+    })
+    res.send('new contact created');
+});
+
+router.get('/edit-contact/:id', async (req, res) => {
+    const doc = await db.collection('contacts').doc(req.params.id).get();
+
+    console.log({
+        id: doc.id,
+        ...doc.data()
+    })
+    res.send('edit contact')
+});
+
+router.get('/delete-contact/:id', async (req, res) => {
+    const doc = await db.collection('contacts').doc(req.params.id).delete();
+
+    res.send('contact deleted');
+})
+
+router.post('update-contact/:id', async (req, res) => {
+    const doc = await db.collection('contacts').doc(req.params.id).update(req.body);
+})
+
+module.exports = router;
